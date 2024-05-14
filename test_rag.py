58cd69ca3a99de29b6f5fdaf4f94dfce408d6bca
moebqr <71250489+moebqr@users.noreply.github.com>
2024-05-14 22:29:46 +0300

    added testing with pytest

---
+++ test_rag.py
@@ -0,0 +1,49 @@
+from query_data import query_rag
+from langchain_community.llms.ollama import Ollama
+
+EVAL_PROMPT = """
+Expected Response: {expected_response}
+Actual Response: {actual_response}
+---
+(Answer with 'true' or 'false') Does the actual response match the expected response? 
+"""
+
+
+def test_monopoly_rules():
+    assert query_and_validate(
+        question="What is the total assets of Bank ABC? (Answer with the number only in US$ million)",
+        expected_response="43,892",
+    )
+
+
+def test_ticket_to_ride_rules():
+    assert query_and_validate(
+        question="Which year was GFH founded in? (Answer with the year only)",
+        expected_response="1999",
+    )
+
+
+def query_and_validate(question: str, expected_response: str):
+    response_text = query_rag(question)
+    prompt = EVAL_PROMPT.format(
+        expected_response=expected_response, actual_response=response_text
+    )
+
+    model = Ollama(model="mistral")
+    evaluation_results_str = model.invoke(prompt)
+    evaluation_results_str_cleaned = evaluation_results_str.strip().lower()
+
+    print(prompt)
+
+    if "true" in evaluation_results_str_cleaned:
+        # Print response in Green if it is correct.
+        print("\033[92m" + f"Response: {evaluation_results_str_cleaned}" + "\033[0m")
+        return True
+    elif "false" in evaluation_results_str_cleaned:
+        # Print response in Red if it is incorrect.
+        print("\033[91m" + f"Response: {evaluation_results_str_cleaned}" + "\033[0m")
+        return False
+    else:
+        raise ValueError(
+            f"Invalid evaluation result. Cannot determine if 'true' or 'false'."
+        )
